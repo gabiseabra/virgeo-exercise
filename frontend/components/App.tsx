02d8bf5911c1a9377bee7e27523c39025e26f80d
Gabriela Seabra <hey@gabiseabra.dev>
2025-01-11 17:37:49 +0100

    setup leaflet

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,10 +1,10 @@
 import * as Styles from './App.module.scss';
+import { Map } from './Map';
 
 export default function App() {
   return (
     <div className={Styles.App}>
-      <h1>My App</h1>
-      <p>Welcome to my app!</p>
+      <Map />
     </div>
   );
 }
