b90fef908207b3fce9b53277c22b768f75c72885
Gabriela Seabra <hey@gabiseabra.dev>
2025-01-19 19:50:52 +0100

    test redirect to login on 401

--- frontend/test/home.test.tsx
+++ frontend/test/home.test.tsx
@@ -19,4 +19,17 @@ describe('/', () => {
 
     expect(location.current?.pathname).toBe('/');
   });
+
+  it('should redirect to /login if the initial request is unauthorized', async () => {
+    mockLocalStorage({ 'ACCESS_TOKEN': { accessToken: 'token' } });
+    fetchMock.get(/.*/, {
+      status: 401,
+      body: { error: 'Unauthorized' },
+    });
+    const { location } = renderRoute('/');
+
+    await flushPromises();
+
+    expect(location.current?.pathname).toBe('/login');
+  });
 });
